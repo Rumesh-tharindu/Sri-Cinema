98799a9a65a7dd9b7f1126ccb5edf6422fb700be
rumesh-tharindu <rumeshtharindu2020@gmail.com>
2020-10-04 19:28:04 +0530

    change header style and bannerslider

--- src/component/Home/BannerSlider.js
+++ src/component/Home/BannerSlider.js
@@ -9,19 +9,21 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
         <div>
            <div className="bannerSlider">
            <OwlCarousel items={2}  
-          className="owl-theme"  
-          loop  
+          loop={true}  
           nav={false}
-          margin={10}
-          dots={false}
-          slideBy={1}
-          autoPlay={true}
-          autoplayTimeout={1000}
-          autoplaySpeed={100} >  
-           <div ><img   src="https://i.pinimg.com/originals/c0/76/8e/c0768ec92145ec5d0631bc2e7a035c1f.jpg"/></div>  
-           <div><img   src="https://i.pinimg.com/originals/c0/76/8e/c0768ec92145ec5d0631bc2e7a035c1f.jpg"/></div>  
-           <div><img  src="https://i.pinimg.com/originals/c0/76/8e/c0768ec92145ec5d0631bc2e7a035c1f.jpg"/></div>  
-           <div><img   src="https://i.pinimg.com/originals/c0/76/8e/c0768ec92145ec5d0631bc2e7a035c1f.jpg"/></div>  
+          autoplay={true}
+          responsiveClass={true}
+          autoplayTimeout={5000}
+          autoplaySpeed={2000}
+          autoplayHoverPause={false}
+          margin={15}
+          stagePadding={200}
+          dots={false} >  
+           <div >
+               <img style={{height:'100%'}}   src="https://i.pinimg.com/originals/c0/76/8e/c0768ec92145ec5d0631bc2e7a035c1f.jpg" alt="one"/> </div>  
+           <div><img   src="https://english.mathrubhumi.com/polopoly_fs/1.4675470.1586324787!/image/image.jpg_gen/derivatives/landscape_894_577/image.jpg " alt="two"/></div>  
+           <div><img  src="https://i.pinimg.com/originals/c0/76/8e/c0768ec92145ec5d0631bc2e7a035c1f.jpg" alt="two"/></div>  
+           <div><img   src="https://i.pinimg.com/originals/c0/76/8e/c0768ec92145ec5d0631bc2e7a035c1f.jpg" alt="four"/></div>  
           
       </OwlCarousel>  
            </div>
